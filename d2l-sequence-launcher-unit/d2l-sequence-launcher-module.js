6ab56aa162bc2418da3211c7dc7fb6267080295a
ilaidlaw <laidlaw434@gmail.com>
2020-03-19 09:41:29 -0500

    add dom if to block rendering and requests

--- d2l-sequence-launcher-unit/d2l-sequence-launcher-module.js
+++ d2l-sequence-launcher-unit/d2l-sequence-launcher-module.js
@@ -207,15 +207,17 @@ class D2LSequenceLauncherModule extends ASVFocusWithinMixin(PolymerASVLaunchMixi
 				<div id ="startDate">[[startDate]]</div>
 			</div>
 			<ol>
-				<template is="dom-repeat" items="[[subEntities]]" as="childLink">
-					<li on-click="_onActivityClicked" class$="[[_padOnActivity(childLink)]]">
-						<template is="dom-if" if="[[_isActivity(childLink)]]">
-							<d2l-activity-link last-module$="[[lastModule]]" is-sidebar$="[[isSidebar]]" href="[[childLink.href]]" token="[[token]]" current-activity="{{currentActivity}}" on-sequencenavigator-d2l-activity-link-current-activity="childIsActiveEvent"></d2l-activity-link>
-						</template>
-						<template is="dom-if" if="[[!_isActivity(childLink)]]">
-							<d2l-inner-module href="[[childLink.href]]" token="[[token]]" current-activity="{{currentActivity}}" on-sequencenavigator-d2l-inner-module-current-activity="childIsActiveEvent"></d2l-inner-module>
-						</template>
-					</li>
+				<template is="dom-if" if="[[_allowChildrenRendering]]">
+					<template is="dom-repeat" items="[[subEntities]]" as="childLink">
+						<li on-click="_onActivityClicked" class$="[[_padOnActivity(childLink)]]">
+							<template is="dom-if" if="[[_isActivity(childLink)]]">
+								<d2l-activity-link last-module$="[[lastModule]]" is-sidebar$="[[isSidebar]]" href="[[childLink.href]]" token="[[token]]" current-activity="{{currentActivity}}" on-sequencenavigator-d2l-activity-link-current-activity="childIsActiveEvent"></d2l-activity-link>
+							</template>
+							<template is="dom-if" if="[[!_isActivity(childLink)]]">
+								<d2l-inner-module href="[[childLink.href]]" token="[[token]]" current-activity="{{currentActivity}}" on-sequencenavigator-d2l-inner-module-current-activity="childIsActiveEvent"></d2l-inner-module>
+							</template>
+						</li>
+					</template>
 				</template>
 			</ol>
 		</d2l-labs-accordion-collapse>
