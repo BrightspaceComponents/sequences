d97e38602ad11a4669e6812b698b5f8a16b3d7f0
Ian McDonald <Ian.McDonald@D2L.com>
2020-07-06 08:37:36 -0400

    move import statement to top

--- components/d2l-sequences-content-video.js
+++ components/d2l-sequences-content-video.js
@@ -1,6 +1,6 @@
+import '@d2l/video/d2l-video.js';
 import '../mixins/d2l-sequences-automatic-completion-tracking-mixin.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
-import '@d2l/video/d2l-video.js';
 export class D2LSequencesContentVideo extends D2L.Polymer.Mixins.Sequences.AutomaticCompletionTrackingMixin() {
 	static get template() {
 		return html`
